a883ccb739526e7cc190f68400adfbf516d72eef
BlackwaveZero <61608293+BlackwaveZero@users.noreply.github.com>
2020-02-29 05:49:06 +0330

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,21 @@
+text=input()
+key=int(input())
+tmp=''
+for c in text:
+    tmp+= chr( (ord(c) + key ) % 256 )
+print (tmp)
+
+#################
+# Function
+#################
+# def convertChar(c,offset):
+#     return chr( (ord(c) + offset ) % 256 )
+# def convert(text,key):
+#     tmp=''
+#     for c in text:
+#         tmp+=convertChar(c,key)
+#     return tmp
+#################
+# Note : why this type of encryptuin is not secure!?
+#################
+
