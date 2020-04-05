ab23429d950b841572b553cf035421bb301f5bb2
BlackwaveZero <61608293+BlackwaveZero@users.noreply.github.com>
2020-04-05 21:33:48 +0430

    Update fibonacciString.py

--- fibonacciString.py
+++ fibonacciString.py
@@ -1,31 +1,20 @@
-#write a program that get a number (n) and prints a string like S1,S2,S3,S4,...,Sn
-#and Si is either + or -
-#Si is positive when i is a fibonacci number otherwise Si is negetive
-#Example:
-#input : 15
-#outpur : +++-+--+----+--
-
-#input : 4
-#outpur : +++-
-
-
+#nokte : dar in ravesh halgheye while namikhahad az 1 ta n bar becharkhad be tedade bozorg tarin adade donbaleye fibonacci kochiktar mosavie n micharkhad
+#masalan agar n 20 vared shavad tanha be tedade (1,2,3,5,8,13) yani 6 bar micharkhad 
+#menha ha ra dar har dor haleghe be tedade faseleye do adade fibonacci chap karde va jaye khode adade fibonacci mosabt gozashte
+#dar inn ravesh az hadeaghal dor haleghe estefade shod
+#ba in raveshe code nevisi memory kamtari estefade mishavad va sorat bala tar miravad
+#mohem hala felan in baraye shoma nist vali goftm ke bedanid
 n=int(input())
 output='+'
 a=1
 b=1
-while True:
-    if b+a>=n:
-        output+='-'*(n-b)
-        break
+while a+b<=n:
     #produce fibonacci
     tmp=b
     b+=a
     a=tmp
-    ############
+    #add symbols
     output+='-'*(b-a-1)
     output+='+'
+output+='-'*(n-b)
 print (output)
-
-
-
-
